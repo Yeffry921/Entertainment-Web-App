bb6d488de95d59ce0262c9b2d934e6159530fb66
Yeffry <yeffry45@gmail.com>
2022-11-12 15:18:55 -0500

    add routing for main pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/Home/Home";
+import Movies from "./pages/Movies/Movies";
+import TvSeries from "./pages/TVSeries/TVSeries";
+import Bookmarks from "./pages/Bookmarks/Bookmarks";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/tvseries",
+        element: <TvSeries />,
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// Layout whole page which will be
