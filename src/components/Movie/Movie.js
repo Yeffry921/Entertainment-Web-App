a382b1853a9b026a7d30a042fff43ce3ee3fabce
Yeffry <yeffry45@gmail.com>
2022-11-17 14:40:40 -0500

    add bookmark icon

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,13 @@
 import React from "react";
-import styles from './Movie.module.css'
+import styles from "./Movie.module.css";
+import { FaRegBookmark } from "react-icons/fa";
 
 const Movie = ({ item }) => {
   return (
-    <div className={styles['movie-container']}>
+    <div className={styles["movie-container"]}>
+      <button className={styles.bookmark}>
+        <FaRegBookmark fill="#fff" className={styles['bookmark-icon']}/>
+      </button>
       <img
         className={styles.movie}
         src={item.thumbnail.trending?.large}
