b6f12c4c754f709047c8e0667340b675f85bfbc8
Yeffry <yeffry45@gmail.com>
2022-11-17 22:14:23 -0500

    add hover button for play

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styles from "./Movie.module.css";
 import { FaRegBookmark } from "react-icons/fa";
+import playSvgIcon from "../../assets/icon-play.svg";
 
 const Movie = ({ item }) => {
   return (
     <div className={styles["movie-container"]}>
+      <div className={styles.overlay}></div>
       <button className={styles.bookmark}>
-        <FaRegBookmark fill="#fff" className={styles['bookmark-icon']}/>
+        <FaRegBookmark fill="#fff" className={styles["bookmark-icon"]} />
       </button>
       <img
         className={styles.movie}
         src={item.thumbnail.trending?.large}
         alt={item?.title}
       />
+      <button className={styles["play-btn"]}>
+        <img src={playSvgIcon} alt="" />
+        <span>Play</span>
+      </button>
     </div>
   );
 };
