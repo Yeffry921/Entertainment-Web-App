014d66d5e19234a75e0181351ea33619e1387ce6
Yeffry <yeffry45@gmail.com>
2022-11-23 18:24:47 -0500

    add Showcase to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import Search from "../../components/Search/Search"
 import Slider from "../../components/Slider/Slider"
+import Showcase from "../../components/Showcase/Showcase"
 
 import styles from '../Home/Home.module.css'
 
@@ -8,6 +9,7 @@ const Home = () => {
     <section className={styles.container}>
       <Search />
       <Slider />
+      <Showcase title="Recommended for you"/>
     </section>
   )
 }
