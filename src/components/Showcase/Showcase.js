0f808bf36434d458406df9982e44e3449ea999dc
Yeffry <yeffry45@gmail.com>
2022-11-27 23:34:01 -0500

    update heading use separate component

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,6 +1,7 @@
 import data from "../../data.json";
 import Bookmark from "../Bookmark/Bookmark";
 import Metadata from "../Metadata/Metadata";
+import ShowcaseHeading from "../ShowcaseHeading/ShowcaseHeading";
 
 import styles from "./Showcase.module.css";
 
@@ -8,8 +9,7 @@ const Showcase = ({ title }) => {
   const recommended = data.filter((movie) => movie.isTrending === false);
   return (
     <section className={styles.container}>
-      <h1>{title}</h1>
-
+      <ShowcaseHeading text={title} />
       <div className={styles.showcase}>
         {recommended.map((movie) => {
           return (
