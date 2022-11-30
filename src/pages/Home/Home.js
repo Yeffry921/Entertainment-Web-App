e04833e22e0207afec03ea240dae084edb709e2f
Yeffry <yeffry45@gmail.com>
2022-11-30 16:37:45 -0500

    remove Search component from Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,12 @@
-import Search from "../../components/Search/Search"
 import Slider from "../../components/Slider/Slider"
 import Showcase from "../../components/Showcase/Showcase"
-
+import data from '../../data.json'
 import styles from '../Home/Home.module.css'
 
 const Home = () => {
+  
   return (
     <section className={styles.container}>
-      <Search />
       <Slider />
       <Showcase title="Recommended for you"/>
     </section>
