38bb908f0766e01bbc819d48d4becd3e35f4c788
Yeffry <yeffry45@gmail.com>
2022-12-01 13:18:10 -0500

    add state logic to track input changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,15 @@
 import styles from "../Search/Search.module.css";
 import { ReactComponent as SearchLogo } from "../../assets/icon-search.svg";
+import { useState } from "react";
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    getQuery(searchTerm)
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -9,7 +17,12 @@ const Search = () => {
           <button type="submit">
             <SearchLogo className={styles.logo} />
           </button>
-          <input type="search" placeholder="Search for movies or TV series" />
+          <input
+            type="search"
+            placeholder="Search for movies or TV series"
+            onChange={handleSearch}
+            value={searchTerm}
+          />
         </div>
       </div>
     </div>
