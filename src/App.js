11ff309e61fe1d978975542f2aa3a596da86984e
Yeffry <yeffry45@gmail.com>
2022-12-01 13:14:42 -0500

    add functionality to search for films/tv shows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Search from "./components/Search/Search";
+import { useState } from "react";
+import Showcase from "./components/Showcase/Showcase";
+import data from "./data.json";
 
 const App = () => {
+  const [filteredData, setFilteredData] = useState([])
+  const getQueryTerm = (term) => {
+    searchedItems(term)
+  }
+
+  const searchedItems = (searchTerm) => {
+    setTimeout(() => {
+      const filtered = data.filter((items) =>
+        items.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filtered)
+    }, 1000);
+  };
+  console.log(filteredData)
+
   return (
     <main className="App">
       <Navigation />
-      <Outlet />
+      <Search getQuery={getQueryTerm}/>
+      {filteredData ? (
+        <Showcase title="Search Results" content={filteredData}/>
+      ) : (
+        <Outlet />
+      )}
     </main>
   );
 };
