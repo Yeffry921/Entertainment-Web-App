bf9b12fd40347c5dfbe8c91e172dbc1382acfef7
Yeffry <yeffry45@gmail.com>
2022-12-02 12:03:36 -0500

    update slider prop data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,18 @@
-import Slider from "../../components/Slider/Slider"
-import Showcase from "../../components/Showcase/Showcase"
-import data from '../../data.json'
-import styles from '../Home/Home.module.css'
+import Slider from "../../components/Slider/Slider";
+import Showcase from "../../components/Showcase/Showcase";
+import data from "../../data.json";
+import styles from "../Home/Home.module.css";
 
 const Home = () => {
-  
+  const trendingData = data.filter((movie) => movie.isTrending);
+  const recommended = data.filter((movie) => movie.isTrending === false);
+
   return (
     <section className={styles.container}>
-      <Slider />
-      <Showcase title="Recommended for you"/>
+      <Slider content={trendingData} />
+      <Showcase title="Recommended for you" content={recommended} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
